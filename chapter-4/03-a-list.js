81e46b86a9970693afd614f6c993bb1dfe17946d
Joey Reyes <jreyes88@gmail.com>
2019-01-10 21:19:10 -0600

    Completed Exercise 3 of Chapter 4

--- chapter-4/03-a-list.js
+++ chapter-4/03-a-list.js
@@ -1,6 +1,5 @@
 /*
   A List
-
   Objects, as generic blobs of values can be used to build all sorts of data structures. A common data structure is the list (not to be confused with the array). A list is a nested set of objects, with the first object holding a reference to the second, the second to the third, and so on.
 
   var list = {
